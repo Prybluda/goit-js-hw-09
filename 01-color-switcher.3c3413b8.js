0ddbb7b5ae10b356840b1f74cf3107dd35bd6d43
Prybluda <134804799+Prybluda@users.noreply.github.com>
2023-10-15 20:34:13 +0000

    Deploying to gh-pages from @ Prybluda/goit-js-hw-09@014d1ffdfd0519f4e7c7ac95d90b1f42120bcfea 🚀

---
+++ 01-color-switcher.3c3413b8.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.disabled=!0,t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval(a,500)})),e.addEventListener("click",(function(){return t.disabled=!1,clearInterval(n),e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.3c3413b8.js.map
